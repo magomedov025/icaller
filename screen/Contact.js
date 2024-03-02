eb2f080d708e9fbb66236c2e8bd79b414dbd6ff1
il <il@MacBook-Pro-il.local>
2024-03-02 18:07:13 +0300

    test commit

--- screen/Contact.js
+++ screen/Contact.js
@@ -1,83 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, Button, View, SafeAreaView, ScrollView,  StatusBar } from 'react-native';
 import * as Contacts from 'expo-contacts'
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react'
+import {
+	Button,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native'
 
 const Contact = ({ navigation }) => {
-  let [error, setError] = useState(undefined);
-  let [contacts, setContacts] = useState(undefined);
-    useEffect(() => {
-      (async () => {
-        const { status } = await Contacts.requestPermissionsAsync();
-        if (status === "granted") {
-          const { data } = await Contacts.getContactsAsync({
-            fields: [Contacts.Fields.FirstName, Contacts.Fields.PhoneNumbers]
-          });
+	let [error, setError] = useState(undefined)
+	let [contacts, setContacts] = useState(undefined)
+	useEffect(() => {
+		;(async () => {
+			const { status } = await Contacts.requestPermissionsAsync()
+			if (status === 'granted') {
+				const { data } = await Contacts.getContactsAsync({
+					fields: [Contacts.Fields.FirstName, Contacts.Fields.PhoneNumbers],
+				})
 
-          if (data.length > 0) {
-            setContacts(data);
-          } else {
-            setError("No contacts found");
-          }
-        } else {
-          setError("Permission to access contacts denied.");
-        }
-      })();
-    }, []);
-    let getContactData = (data, property) => {
-      if (data) {
-        return data.map((data, index) => {
-          return (
-            <View key={index}>
-              <Text>{data[property]}</Text>
-            </View>
-          )
-        });
-      }
-    }
-    let getContactRows = () => {
-      if (contacts !== undefined) {
-        return contacts.map((contact, index) => {
-          return (
-            <View key={index} style={styles.contact}>
-              <Text>{contact.firstName}</Text>
-              {getContactData(contact.phoneNumbers, "number")}
-            </View>
-          );
-        });
-      } else {
-        return <Text>Awaiting contacts...</Text>
-      }
-    }
+				if (data.length > 0) {
+					setContacts(data)
+				} else {
+					setError('No contacts found')
+				}
+			} else {
+				setError('Permission to access contacts denied.')
+			}
+		})()
+	}, [])
+	let getContactData = (data, property) => {
+		if (data) {
+			return data.map((data, index) => {
+				return (
+					<View key={index}>
+						<Text>{data[property]}</Text>
+						<View></View>
+					</View>
+				)
+			})
+		}
+	}
+	let getContactRows = () => {
+		if (contacts !== undefined) {
+			return contacts.map((contact, index) => {
+				return (
+					<View key={index} style={styles.contact}>
+						<Text>{contact.firstName}</Text>
+						{getContactData(contact.phoneNumbers, 'number')}
+					</View>
+				)
+			})
+		} else {
+			return <Text>Awaiting contacts...</Text>
+		}
+	}
 
-    
+	const loadScene = () => {
+		navigation.navigate('HomeScreen')
+	}
 
-
-
-    const loadScene = () => {
-        navigation.navigate('HomeScreen');
-      }
-
-      return (
-        <View style={styles.container}>
-          <Button title='Открыть страницу' onPress={loadScene} />
-          <ScrollView>
-            {getContactRows()}
-          </ScrollView>
-          <Text>{error}</Text>
-          <StatusBar style="auto" />
-        </View>
-      )
+	return (
+		<View style={styles.container}>
+			<Button title='Открыть страницу' onPress={loadScene} />
+			<ScrollView>{getContactRows()}</ScrollView>
+			<Text>{error}</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  contact: {
-    marginVertical: 8,
-    width: '100%'
-  }
-});
+	contact: {
+		marginVertical: 8,
+		width: '100%',
+	},
+})
 
 export default Contact
-
-
